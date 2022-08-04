ae45242d4236b66621cdf61482e2ef3a2f5ab362
lean120892 <lean120892@gmail.com>
2022-08-04 12:09:12 -0300

    controlar

--- src/components/funciones/Funciones.js
+++ src/components/funciones/Funciones.js
@@ -16,7 +16,8 @@ function getProductos(time) {
 
 export{getProductos};
 */
-import { collection, getDocs,query,where } from "firebase/firestore";
+import { collection, getDocs} from "@firebase/firestore";
+import { doc,getDoc} from "firebase/firestore";
 import {db} from './Firebase'
 
 //Obtener todos los productos
@@ -32,18 +33,18 @@ return dataFromFirestore
 }
 
 // Obtener un producto mediante ID
-const FirebaseOneProduct = async (id)=>{
-let q;
-if(id){
-    q= query(collection(db, "Productos"), where ('categoryId', '==', id))
-    console.log(q)
-}
-const querySnapshot = await getDocs(q);
-const dataFromFirestore = querySnapshot.docs.map(doc =>({
-        id: doc.id,
-        ...doc.data()
-    }))
-    return dataFromFirestore;
+const FirebaseOneProduct = async (idItem)=>{
+    const docRef = doc(db, "Productos",idItem);
+    const docSnap = await getDoc(docRef);
+    if(docSnap.exists()){
+        return{
+            id: idItem,
+            ...docSnap.data()
+        }
+    } else{
+        console.log("No se encontro el elemento")
+    }
+
 }
     
 
